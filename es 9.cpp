46c105718afbfa7d45d19e56e562be040857c34f
Ferris The Railway Otter <ferristherailwayotter@gmail.com>
2024-12-16 10:06:51 +0100

    Add files via upload

---
+++ es 9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int Numero, i;
+    float Soglia, Media, Somma;
+    i=0;
+    Somma=0;
+
+    do {
+        cout << "Inserire un numero maggiore di 0" <<endl;
+        cin >> Numero;
+    } while (Numero<0);
+
+    cout << "Inserire " <<Numero;
+    cout << " Numeri" <<endl;
+    while (i != Numero) {
+        cout << "Inserisci un numero minore o uguale di " <<Numero <<endl;
+        cin >> Soglia;
+        if (Soglia>Numero) {
+            cout << "VALORE MAGGIORE DI " <<Numero <<endl;
+        } else  {
+            Somma = Somma + Soglia;
+            i= i+1;
+        }
+    }
+    Media = Somma/Numero;
+    cout <<"La media e' " <<Media <<endl;
+    return 0;
+}
